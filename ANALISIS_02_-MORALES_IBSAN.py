16ccb013548b361b16cf181705f2053a71414b9f
Ibsanmorales <ibsan_89@hotmail.com>
2020-10-01 08:26:08 -0700

    Proyecto con resumen de transportes, total venta y numero de exports e imports por ruta

--- ANALISIS_02_-MORALES_IBSAN.py
+++ ANALISIS_02_-MORALES_IBSAN.py
@@ -4,6 +4,74 @@ Created on Mon Sep 28 06:33:09 2020
 
 @author: ibsan
 """
-import csv
+import os
+import data_services
+
+exit = False;
+while exit == False:
+    print("**********************************************")
+    print("MENU DE CONSULTA")
+    print("**********************************************")
+    print("1.- Top 10 Exportacion Importacion")
+    print("2.- Top 3 Medio de Transporte")
+    print("3.- Top Importaciones Exportaciones por Pais")
+    print("4.- Salir")
+    print("\n")
+    menu_item_select = input("Introdusca la opcion deseada: ")
+    #Comparaciones para realizar los proceso correspondientes al menu seleccionado
+    if(menu_item_select == '1'):
+      regresar_programa = 'n'
+      while regresar_programa == 'n':
+        #os.system('clear')
+        print("**********************************************")
+        print("Top Exportacion")
+        print("**********************************************")
+        data_services.getTopTenExportImport("Exports")  
+        print("**********************************************")
+        print("Top Importaciones")
+        print("**********************************************")
+        data_services.getTopTenExportImport("Imports")
+        print("\n")
+        regresar_programa = data_services.yes_or_no_regresar("Salir de consulta o realizar nuevamente la consulta ")
+      #os.system('clear')
+    elif(menu_item_select == "2"):
+      regresar_programa = 'n'
+      while regresar_programa == 'n':
+        #os.system('clear')
+        print("**********************************************")
+        print("Top 3 Medio de Transporte Exportaciones")
+        print("**********************************************")
+        data_services.getTopThreeTrasnportImportExport("Exports")
+        print("**********************************************")
+        print("Top 3 Medio de Transporte Importaciones")
+        print("**********************************************")
+        data_services.getTopThreeTrasnportImportExport("Imports")
+        print("\n")
+        regresar_programa = data_services.yes_or_no_regresar("Salir de consulta o realizar nuevamente la consulta ")
+      #os.system('clear')
+    elif(menu_item_select == "3"):
+      regresar_programa = 'n'
+      while regresar_programa == 'n':
+       #os.system('clear')
+        print("**********************************************")
+        print("Top 80% Exportaciones valor total por Pais")
+        print("**********************************************")
+        data_services.getTotalImportacionesExportacionesPais("Exports")
+        print("**********************************************")
+        print("Top 80% Importaciones valor total por Pais")
+        print("**********************************************")
+        data_services.getTotalImportacionesExportacionesPais("Imports")
+        print("\n")
+        regresar_programa = data_services.yes_or_no_regresar("Salir de consulta o realizar nuevamente la consulta ")
+      #os.system('clear')
+    elif(menu_item_select == "4"):
+      #os.system('clear')
+      exit = True
+    else:
+      #os.system('clear')
+      print("\n")
+      print("**** ERROR: Seleccione una Opcion valida ****")
+      print("\n")
+
 
 
---
+++ data_services.py
@@ -0,0 +1,70 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep 28 06:41:24 2020
+
+@author: ibsan
+"""
+#importacion de libreria pandsa
+import pandas as pd
+#variable para asignacion de archivo con informacion
+archivo_csv = 'synergy_logistics_database.csv'
+
+#Metodo para regresar a menu
+def yes_or_no_regresar(question):
+    while "the answer is invalid":
+      #os.system('clear')
+      reply = str(input(question+' (y/n): ')).lower().strip()
+      if reply[0] == 'y':
+          return 'y'
+      if reply[0] == 'n':
+          return 'n'
+#Metodo para Generar resumen en consola y creacion de archivo en excel de informacion de top Exports Imports  
+def getTopTenExportImport(direction):
+    #lectura y guardado de informacion de archivo en memoria
+    data = pd.read_csv(archivo_csv)
+    #filtro y query de informacion 
+    grouped = data[data['direction'] == direction].groupby(['origin', 'destination']).size().reset_index().rename(columns={0:'Cantidad'}).sort_values(by=['Cantidad'], ascending=False).head(10).reset_index()
+    #imprecion en consola de informacion
+    print(grouped[['origin','destination','Cantidad']])
+    #generacion de archivo excel
+    prueba = pd.DataFrame(grouped[['origin','destination','Cantidad']])
+    prueba.to_excel('1resumen'+direction+'.xlsx', sheet_name='top10'+direction, index=False)
+    
+# Metodo para generacion resumen en consola y creacion de archivo en excel de informacion transportes Exports Imports
+def getTopThreeTrasnportImportExport(direction):
+    #lectura y guardado de informacion de archivo en memoria
+    data = pd.read_csv(archivo_csv)
+    #filtrado y query de informacion
+    grouped = data[data['direction'] == direction].groupby(['transport_mode']).sum().sort_values(by=['total_value'], ascending=False).reset_index()
+    #impresion de informacion en consola
+    print(grouped[['transport_mode','total_value']])
+    #grabado de archico excel
+    prueba = pd.DataFrame(grouped[['transport_mode','total_value']])
+    prueba.to_excel('2resumen'+direction+'.xlsx', sheet_name='transport'+direction, index=False)
+   
+# Metodo para genera resumen en consola y creacion de archivo en excel de informacion total por pais Exports Imports
+def getTotalImportacionesExportacionesPais(direction):
+    #varaibles locales 
+    suma_total_values_temp = 0
+    suma_total_values = 0
+    select_value_final = 0
+    #lectura y guardado de informacion de archivo en memoria
+    data = pd.read_csv(archivo_csv)
+    #filtrado y query de informacion
+    grouped = data[data['direction'] == direction].groupby(['origin', 'destination']).sum().sort_values(by=['total_value'], ascending=False).reset_index()
+    #suma de totales y obtener el 80% del total
+    suma_total_values_temp = grouped['total_value'].sum() * .8
+    #comparacion para saber en que registro de total de obtiene el 80% de las ventas
+    for valueTotal in grouped['total_value']:
+        if(suma_total_values <= suma_total_values_temp):
+            suma_total_values += valueTotal
+            select_value_final = valueTotal
+    #print(select_value_final)
+    #print(suma_total_values_temp)
+    data_final = grouped[['origin','destination','total_value']]
+    #imprime en consola con validacion del total value mayor o igual al obtenido del 80%
+    print(data_final[data_final['total_value'] >= select_value_final])
+    #grabado de archivo excel
+    prueba = pd.DataFrame(data_final[data_final['total_value'] >= select_value_final])
+    prueba.to_excel('3resumen'+direction+'.xlsx', sheet_name='total_value'+direction, index=False )
+   
